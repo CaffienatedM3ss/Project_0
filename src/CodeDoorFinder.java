0ca25db1fc4ba9078ab5d577ea7089e17feb9c27
Shy <s.daniell05@outlook.com>
2026-01-16 14:06:57 -0500

    Door

--- src/CodeDoorFinder.java
+++ src/CodeDoorFinder.java
@@ -32,6 +32,16 @@ public class CodeDoorFinder {
 
     public static void main(String[] args) {
 
+
+
+    }
+
+    public static void findDoor(CodeDoorWall wall) {
+
+    }
+
+    public static boolean findDoor(CodeDoorWall wall) {
+
     }
 
     //constructor
